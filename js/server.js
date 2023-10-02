ca4e2c134f40491168f8e64b78c6131d85c63aaf
diego <di.nevens@duocuc.cl>
2023-10-01 22:56:42 -0300

    Varios Rollbacks

--- js/server.js
+++ js/server.js
@@ -1,40 +1,14 @@
-// const http = require('http');
+const http = require('http');
  
-// const hostname = '127.0.0.1';
-// const port = 3000;
+const hostname = '127.0.0.1';
+const port = 3000;
  
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Prueba Server Local Node.JS');
-// });
- 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-// server.js (en tu servidor web)
-const express = require('express');
-const { Pool } = require('pg');
-const app = express();
-
-// Configura el pool de PostgreSQL
-const pool = new Pool({
-  connectionString: 'postgres://zplgxrfq:APvDvrAQdcb52ADYBWT_P2OUH3UaP-Rc@motty.db.elephantsql.com/zplgxrfq',
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Prueba Server Local Node.JS');
 });
-
-// Ruta para la API que realiza la consulta
-app.get('/api/categoria_prod', (req, res) => {
-  pool.query('SELECT * FROM categoria_prod', (error, result) => {
-    if (error) {
-      console.error('Error al ejecutar la consulta', error);
-      res.status(500).json({ error: 'Error al obtener los datos' });
-    } else {
-      res.json(result.rows);
-    }
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Servidor web iniciado en el puerto 3000');
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
