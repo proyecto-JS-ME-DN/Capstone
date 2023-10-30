6dbb6da610bd0ea04eb0da6cecc019db784b42d7
JoseDuoc1 <jo.sepulvedaq@duocuc.cl>
2023-10-29 21:21:53 -0300

    cambio tabla

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ app.post("/auth", async (req, res) => {
 app.post("/servicio", async (req, res) => {
   const {nombre,correo,patente,marca,tipo,fecha,hora} = req.body;
   pool.query(
-    "INSERT INTO public.agenda(nombre,correo,patente,marca,tipo,fecha,hora) VALUES ($1, $2, $3, $4, $5, $6, $7)", [nombre,correo,patente,marca,tipo,fecha,hora], async (error, results) => {
+    "INSERT INTO public.agenda_externo(nombre,correo,patente,marca,tipo,fecha,hora) VALUES ($1, $2, $3, $4, $5, $6, $7)", [nombre,correo,patente,marca,tipo,fecha,hora], async (error, results) => {
       if (error) {
         console.log(error);
       } else {
