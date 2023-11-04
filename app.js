2a6712e94e54a797ccfd94af3b918751b72ace2b
diego <di.nevens@duocuc.cl>
2023-11-04 14:10:19 -0300

    Correcion auth

--- app.js
+++ app.js
@@ -66,6 +66,58 @@ app.get("/", (req, res) => {
   }
 });
 
+
+app.post("/auth", async (req, res) => {
+  const user = req.body.user;
+  const pass = req.body.pass;
+  let passwordHaash = await bcryptjs.hash(pass, 8);
+  if (user && pass) {
+    pool.query(
+      'SELECT * FROM public.login WHERE "user" = $1',
+      [user],
+      async (error, results) => {
+        if (
+          results.rows.length == 0 ||
+          !(await bcryptjs.compare(pass, results.rows[0].password))
+        ) {
+          //Alerta despues de guardar
+          res.render("login", {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Usuario y/o contraseña incorrecta",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: "login",
+          });
+        } else {
+          req.session.loggedin = true;
+          req.session.name = results.rows[0].name;
+          res.render("login", {
+            alert: true,
+            alertTitle: "Conexion Exitosa",
+            alertMessage: "Login correcto",
+            alertIcon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: "",
+          });
+        }
+      }
+    );
+  } else {
+    res.render("Advertencia", {
+      alert: true,
+      alertTitle: "Advertencia",
+      alertMessage: "Ingrese un usuario y/o contraseña",
+      alertIcon: "warning",
+      showConfirmButton: true,
+      timer: false,
+      ruta: "login",
+    });
+  }
+});
+
 // Server local
 app.listen(3000, (req, res) => {
   console.log("servidor funcionando en http://localhost:3000");
