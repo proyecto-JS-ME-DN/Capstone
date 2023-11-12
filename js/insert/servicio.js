0d4a0e1bd8eee5b49cfff415db21a753d6fc0875
diego <di.nevens@duocuc.cl>
2023-11-12 19:56:41 -0300

    Cambio Formulario, agregado marcas de autos

--- js/insert/servicio.js
+++ js/insert/servicio.js
@@ -6,27 +6,27 @@ const router = express.Router();
 const pool = require('../../database/db');
 
 router.post("/servicio", async (req, res) => {
-    const {nombre,correo,patente,marca,tipo,fecha,hora} = req.body;
-    pool.query(
-      "INSERT INTO public.agenda_externo(nombre,correo,patente,marca,tipo,fecha,hora) VALUES ($1, $2, $3, $4, $5, $6, $7)", [nombre,correo,patente,marca,tipo,fecha,hora], async (error, results) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const role = req.session.loggedin ? req.session.role : "n_reg";
-          res.render("servicio", {
-            alert: true,
-            alertTitle: "Agenda Registrada",
-            alertMessage: "Agenda Registrada",
-            alertIcon: "success",
-            showConfirmButton: false,
-            timer: 1500,
-            ruta: "",
-            role
-          });
-        }
+  const {nombre,correo,patente,marca,tipo,fecha,hora,descripcion} = req.body;
+  pool.query(
+    "INSERT INTO public.agenda_externo(nombre,correo,patente,marca,tipo,fecha,hora,descripcion) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [nombre,correo,patente,marca,tipo,fecha,hora,descripcion], async (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const role = req.session.loggedin ? req.session.role : "n_reg";
+        res.render("servicio", {
+          alert: true,
+          alertTitle: "Agenda Registrada",
+          alertMessage: "Agenda Registrada",
+          alertIcon: "success",
+          showConfirmButton: false,
+          timer: 1500,
+          ruta: "",
+          role
+        });
       }
-    );
-  });
+    }
+  );
+});
 
 // Aquí puedes exportar el router para usarlo en el archivo principal
 module.exports = router;
